f2d1441094f10b5b72b8277ef9d0b638557163eb
AkashKaranam <ak111@rice.edu>
2021-05-24 19:43:57 -0500

    Extended implementation of permutation generation for a vector of values. However, the length of the list must also be given separately.

---
+++ PermutationGenerator.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+
+using namespace std;
+vector<int> permutation;
+const vector<int> primes({2,3,5});
+bool chosen[4];
+
+void search() {
+    if(permutation.size() == primes.size()) {
+        cout << "THE OUTPUTTED PERMUTATION IS ";
+        for(int i=0; i < permutation.size(); i++)
+            cout << permutation.at(i) << ' ';
+        cout << '\n';
+    }
+    else
+    {
+        for(int i=1; i <= primes.size(); i++) {
+            if(chosen[i]) continue;
+            chosen[i] = true;
+            permutation.push_back(primes.at(i-1));
+            // printPermutation();
+            search();
+            chosen[i] = false;
+            permutation.pop_back();
+            // printPermutation();
+        }
+    }
+}
+
+int main()
+{
+    search();
+}
